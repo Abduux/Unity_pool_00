7efce2dee6c1999ba70e5301d5df369cef9b3ddd
Abduux <abduux.ta@gmail.com>
2022-04-19 12:31:55 +0000

    First_Ex_DONE

---
+++ Unity_00/Assets/Scripts/square.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square : MonoBehaviour
+{
+    public SpriteRenderer Square;
+    void Start()
+    {
+        Square = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Square.color = Color.red;
+          //  print("Change Color");
+        }
+        else
+        Square.color = Color.white;
+    }
+}
